f1e1293e31db76ae5400de05b77fd24f80c4ff95
maedeh82 <maedeh82mahi@gmail.com>
2024-09-16 13:16:01 +0330

    edite 4 hour

--- app.js
+++ app.js
@@ -360,25 +360,29 @@
 // یکیش اینه که به ما نشون میده برنامه در حال اجراس 
 // یکیشم مث الان که نه تست میکنیم نه چیزی فقط داریم کد میزنیم 
 // حالا اگر بخواهیم بفهمیم برنامه تو چه فازی است کافی است کد های زیر را به کد هامون اضافه کنیم
-// const express = require ("express")
-// const app = express()
-// const helmet = require ("helmet")
-// const morgan = require ('morgan')
+const express = require ("express")
+const app = express()
+const helmet = require ("helmet")
+const morgan = require ('morgan')
+const debug = require("debug")('app;main')
 
-// app.use(express.static('public'))
-// app.use (helmet())
+app.use(express.static('public'))
+app.use (helmet())
 
 
-// if(app.get('env') === 'development'){
-//   console.log('morgan is active')
-//   app.use(morgan('tiny'))
-// }
+if(app.get('env') === 'development'){
+  debug('morgan is active')
+  app.use(morgan('tiny'))
+}
 
-// console.log(app.get('env'))
-// const port = process.env.PORT || 3000
-// app.listen(port, ()=> console.log(`listening on port ${port}`))
+console.log(app.get('env'))
+const port = process.env.PORT || 3000
+app.listen(port, ()=> console.log(`listening on port ${port}`))
 // پکیجی است که که به تازگی نصب کردم morgan
 // برای این است که لاگ هامون رو در یک فرمت خیلی خلاصه بیاد اجرا کنه
+
+// برای اینکه مطمئن شویم که در چه محیطی قرار داریم کافیه از کد زیر استفاده کنیم
+// console.log(process.env.NODE-ENV)
 
 
 
@@ -388,9 +392,57 @@
 // npm i config برای نصب این پکیج باید از این کد در قسمت ترمینال استفاده کنیم
 // تنظیمات این کانفیگ ها برای دولوپمنت داخل فولدر کانفیگ در سه فایل انجام شده است
 // حالا برای استفاده از این ماژول کافی است کد های زیر را به کدهای خودمون اضافه کنیم
-const config = require('config')
+// const config = require('config')
 
-console.log("Application Name:" , config.get("name"))
-console.log("Application version:" , config.get("version"))
-console.log("Application SMS:" , config.get("SMS"))
+// console.log("Application Name:" , config.get("name"))
+// console.log("Application version:" , config.get("version"))
+// console.log("sms:" , config.get("SMS.ip"))
+
+
+
+// وقتی بخواهیم یک متغیر رو ست کنیم در قسمت ترمینال کد زیر راه مینویستیم
+// این فرم برای اطلاعات حساسمون است
+// set expressapp_smskey=123456
+// عدد کد بالا برای مثال جای رمزمونه 
+// در قسمت کانفیگ فایل کاستوم یک کد مربوط به این کد است که برای اینکه
+// اطلاعات مهم را در خود صفحه اصلی نریزیم استفاده میشود ان را در متغیر های محیطی میریزیم 
+// و هر وقت خواستیم از ان استفاده میکنیم اینجوری امنیت بالا تری دارد 
+// وقتی هم که بخواهیم اجراش کنیم از کد زیر استفاده میکنیم و به کد های دیگرمون اضافه میکنیم
+// console.log("sms key:" , config.get("SMS.key"))
+
+
+
+// پکیج دیباگ
+// npm i debug برای نصب این بکیج در قسمت ترمینال این کد را باید بزنیم
+// با استفاده از این پکیج میتوانیم لاگ گرفتنمونو هر موثع که خواستیم استفاده نکنیم
+// یا اینکه نزاریم خونده بشه اون کنسول دات لاگی که نوشتیم یه همچین چیزی که انگار غیر فعال میکنیم لاگ هار
+// const debug = require("debug")('app;main')
+// اول باید کد بالا در پروژه باشد بعد میتونیم مث کد پایین ازش استفاده کنیم
+// برای مثال به جای کنسول دات لاگ میایم کد پایین را مینوسیم
+// debug('hello world')
+// برای اینکه دیباگ فعال شود باید از یک متغیر محیطی استفاده کنیم در قسمت ترمینال کد زیر زده شود
+// DEBUG=app:main
+
+
+// >>>>>>>>>>> این کد یک باگ داره که میگه به خاطر فرمت اشتباه در ویندوز <<<<<<<<<<<<
+
+
+// برای اینکه غیر فعال شود متغیر محیطی بالا کافیه کد زیر را در قسمت ترمینال بزنیم
+// DEBUG=
+// یدونه دیگ هم از دیباگ هست که در پایین گذاشتم کدشو
+// dbdebug("hello world")
+// ولی قسمت دیباگ رو مشکل دارم کلا علی باید توضیح بده
+
+
+
+// برای اینکه در پاسخ به درخواستی که میخواد یک صفحه اچ تی ام ال برگردونیم دوتا راه داریم
+// راه اول اینه که یک فایل با پسوند اچ تی ام ال میسازیم
+// را دومش اینه که از ویو انجین استفاده کنیم ویو انجین هم خودش چند تا بخش داره که ما میخوایم از راه
+// ejs استفاده کنیم
+// باید اول پکیجشو نصب کنیم ک کد زیر را در ترمینال وارد میکنیم
+// npm i ejs
+// حالا برای اینکه بتوانیم در کد هامون ازش استفاده کنیم باید کد زیر را در کد هامون قرار دهیم
+// app.set('view engine', 'ejs')
+// کامل نیست کد های بالا  
+
 
